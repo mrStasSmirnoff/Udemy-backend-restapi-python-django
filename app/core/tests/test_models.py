eece1984818e72ae3ee5eb5bafe2e10da5c8302e
mrStasSmirnoff <mr.StasSmirnoff@hotmail.com>
2023-10-29 20:37:14 +0100

    finalizing user creating model, ch9

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -20,4 +20,38 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password ))
+        self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test2@Example.COM', 'test2@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a use without an email raises a Value Error"""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+
+    def test_create_super_user(self):
+        """Test creating a superuser"""
+
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
