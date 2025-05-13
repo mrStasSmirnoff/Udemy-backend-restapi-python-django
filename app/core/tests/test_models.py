dcc46a69068de98258ad9a2ace43e0811b1fa025
mrStasSmirnoff <mr.StasSmirnoff@hotmail.com>
2025-05-13 23:38:18 +0200

    starting with tags api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,6 +6,12 @@ from django.contrib.auth import get_user_model
 from decimal import Decimal
 from core import models
 
+def create_user(email='user@example.com', password='testpass123'):
+    """
+    Helper function to create a new user
+    """
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     """
@@ -71,3 +77,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """
+        Test creating a tag is successful
+        """
+        user = create_user()
+        tag = models.Tag.objects.create(
+            user=user,
+            name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
